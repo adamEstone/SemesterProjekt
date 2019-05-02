54d30d7c89ab2647de9ce362da1198dd4b64f9f1
Adam Aron Edelsten <adamedelsten@hotmail.com>
2019-05-02 19:36:46 +0200

    arrays which will hold loaded data added

--- src/semesterprojekt/ResourceClass.java
+++ src/semesterprojekt/ResourceClass.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package semesterprojekt;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import javax.sound.sampled.Clip;
 
 /**
  *
  * @author Adam
  */
+
 public class ResourceClass {
+    
+    public static java.util.List<BufferedImage> LoadedSprites = new ArrayList<>();
+    public static java.util.List<Clip> LoadedSounds = new ArrayList<>();
+    
+    void resourceClass(){
+    
+    
+    }
+    
     
 }
