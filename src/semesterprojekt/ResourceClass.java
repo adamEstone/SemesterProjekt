1aeb25f125bbea4d2cbf4d4debf94f85783196ee
Adam Aron Edelsten <adamedelsten@hotmail.com>
2019-05-02 20:58:37 +0200

    can now load and keep data in static data arrays

--- src/semesterprojekt/ResourceClass.java
+++ src/semesterprojekt/ResourceClass.java
@@ -1,8 +1,10 @@
-
 package semesterprojekt;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
 import javax.sound.sampled.Clip;
 
 /**
@@ -11,14 +13,39 @@ import javax.sound.sampled.Clip;
  */
 
 public class ResourceClass {
-    
+
     public static java.util.List<BufferedImage> LoadedSprites = new ArrayList<>();
     public static java.util.List<Clip> LoadedSounds = new ArrayList<>();
-    
-    void resourceClass(){
-    
-    
+
+    ResourceClass() {
+        System.out.println("LOADING:");
+        LoadSprite("SpaceShip.png");
     }
-    
-    
+
+    public static void LoadSprite(String spriteString) {//læg
+/*
+        for (int i = 0; i < LoadedSprites.size(); i++) {
+
+            if (LoadedSprites.get(i).toString().equals(spriteString)) {
+
+                return LoadedSprites.get(i);
+            }
+
+        }
+*/
+        //hvis spriten ikke blev fundet i den loaded hukommelse-array
+        try {
+
+            BufferedImage TempImg = ImageIO.read(new File(spriteString));
+            LoadedSprites.add(TempImg);
+            System.out.println("LOADED: "+ spriteString);
+            
+
+        } catch (IOException e) {
+            System.out.println("ERROR LOADING IMAGE");
+            
+        }
+
+    }
+
 }
