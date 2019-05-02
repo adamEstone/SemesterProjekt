5c768a14bd3120d615b6244091aaaf5acd2f36c6
Adam Aron Edelsten <adamedelsten@hotmail.com>
2019-05-02 20:56:29 +0200

    the Player is not drawn in this class any longer but happens inside Gameinstance.draw()

--- src/semesterprojekt/GameInstance.java
+++ src/semesterprojekt/GameInstance.java
@@ -1,5 +1,6 @@
 package semesterprojekt;
 
+import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -8,34 +9,41 @@ import java.util.Date;
  * @author Adam
  */
 public class GameInstance {
+
+    
     
     public static java.util.List<Enemy> enemies = new ArrayList<>();
 
-    private PlayerShip myPlayerShip = new PlayerShip(300, 300, 100, 100);
     boolean ResartGame = false;
+    
+    private PlayerShip myPlayerShip = new PlayerShip(300, 300, 100, 100);
 
     GameInstance() {//constructor  or new game
 
     }
-    
-    private static int a=0;
-    private static long oldMillis=0;
-    
+
+    private static int a = 0;
+    private static long oldMillis = 0;
+
     public static void tickGame() {
 
         var today = new Date();
 
         long millis = System.currentTimeMillis() % 1000;
-        
-        if(millis!=oldMillis){System.out.println(a);a++;}      
+
+        if (millis != oldMillis) {
+            System.out.println(a);
+            a++;
+        }
 
         for (Enemy enemy : enemies) {
             //move enemy()
             //check enemy for collision()
             //make enemy shoot() 
+            //animation?
         }
-        
-                if (enemies.isEmpty()) {
+
+        if (enemies.isEmpty()) {
             System.out.println("NEXT LEVEL");
         } //next level
 
@@ -45,6 +53,17 @@ public class GameInstance {
 
         return ResartGame;
 
+    }
+
+    public static Graphics2D drawGame(Graphics2D bufferedGraphics) {
+        Graphics2D tempG2D = bufferedGraphics;
+        
+        tempG2D.drawImage(
+                ResourceClass.LoadedSprites.get(0), 
+                MultiMuselytter.mouseX-(ResourceClass.LoadedSprites.get(0).getWidth()/2),
+                600, null);
+        
+        return tempG2D ;
     }
 
 }
